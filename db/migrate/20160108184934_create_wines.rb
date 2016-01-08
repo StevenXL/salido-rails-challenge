c9987c9f8a86496e66b72eaad5f8ed9ed54a09d0
Steven Leiva <leiva.steven@gmail.com>
2016-01-08 17:24:42 -0500

    Remove precision on the decimal types in the Wine relation.

--- db/migrate/20160108184934_create_wines.rb
+++ db/migrate/20160108184934_create_wines.rb
@@ -2,9 +2,9 @@ class CreateWines < ActiveRecord::Migration
   def change
     create_table :wines do |t|
       t.string :name, null: false
-      t.decimal :price_min, null: false, precision: 2
-      t.decimal :price_max, null: false, precision: 2
-      t.decimal :price_retail, null: false, precision: 2
+      t.decimal :price_min, null: false
+      t.decimal :price_max, null: false
+      t.decimal :price_retail, null: false
       t.string :year
 
       t.references :appellation, index: true, foreign_key: true
