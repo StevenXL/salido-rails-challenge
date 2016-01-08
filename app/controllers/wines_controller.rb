3873534dfd20dc56b4ac990a5e03eec464bea877
Steven Leiva <leiva.steven@gmail.com>
2016-01-08 18:27:42 -0500

    Add a form for creating a new wine.

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -2,4 +2,11 @@ class WinesController < ApplicationController
   def index
     @wines = Wine.all
   end
+
+  def new
+    @wine = Wine.new
+    @varietals = Varietal.all
+    @vineyards = Vineyard.all
+    @appellations = Appellation.all
+  end
 end
