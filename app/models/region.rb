c7aaba596790d3b0b4a0cc1649cddf6e965b0660
Steven Leiva <leiva.steven@gmail.com>
2016-01-09 13:10:39 -0500

    Add a Regions resource with the ability to view all wines in a region.

--- app/models/region.rb
+++ app/models/region.rb
@@ -2,4 +2,5 @@ class Region < ActiveRecord::Base
   validates :name, presence: true
 
   has_many :appellations
+  has_many :wines, through: :appellations
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :wines
+
+  resources :regions, except: [:update, :edit]
 end
