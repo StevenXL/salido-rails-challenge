c596e35c37865ab941f36058e9bc46ea31742324
Steven Leiva <leiva.steven@gmail.com>
2016-01-09 13:28:02 -0500

    Add ability to create a new Region without duplication in the database.

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -2,4 +2,34 @@ class RegionsController < ApplicationController
   def index
     @regions = Region.all
   end
+
+  def new
+    @region = Region.new
+  end
+
+  def create
+    normalized_params = normalize(region_params)
+
+    @region = Region.find_or_create_by(normalized_params)
+
+    if @region.save
+      flash[:success] = "Region successfully added to or found in database"
+      redirect_to regions_path
+    else
+      flash[:danger] = "Region not saved"
+      render :new
+    end
+  end
+
+  private
+
+  def region_params
+    params.require(:region).permit(:name)
+  end
+
+  def normalize(hash)
+    hash.each_with_object({}) do |(key, value), memo|
+      memo[key] = value.titlecase
+    end
+  end
 end
